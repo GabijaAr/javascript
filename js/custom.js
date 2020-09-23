76b7e8f374bd555e7359cae77a26f2b4f0fc5027
Gabija <gabija.arm@gmail.com>
2020-09-23 10:35:48 +0300

    Initial

--- js/custom.js
+++ js/custom.js
@@ -8,17 +8,29 @@ console.log(unfinished);
 const unknown = 0;
 console.log(unknown);
 
-const done,text = "true";
-console.log(done,text);
-const unfinished,text = "false";
-console.log(unfinished,text);
-const unknown,text = "N";
-console.log(unknown,text);
+const doneText = "true";
+console.log(doneText);
+const unfinishedText = "false";
+console.log(unfinishedText);
+const unknownText = "N";
+console.log(unknownText);
 
 // 3-4 Kintamųjų sąrašo sudarymas
-const forest,birds = "Ciconia nigra"; "Buteo buteo"; "Columba palumbus"; "Cuculus canorus"; "Otus scops";
-console.log(forest,birds);
-const grassLand,birds = "Perdix perdix"; "Bonasa bonasia"; "Tetrao tetrix"; "Syrrhaptes paradoxus"; "Upupa epops";
-console.log(grassLand,birds);
-const waterLand,birds ="Phalacrocorax corbo"; "Gavia stellata"; "Branta bernicle"; "Aix galericulata"; "Aythya fuligula";
-console.log(waterLand,birds)
+const forestBirds = "Ciconia nigra";
+("Buteo buteo");
+("Columba palumbus");
+("Cuculus canorus");
+("Otus scops");
+console.log(forestBirds);
+const grassLandBirds = "Perdix perdix";
+("Bonasa bonasia");
+("Tetrao tetrix");
+("Syrrhaptes paradoxus");
+("Upupa epops");
+console.log(grassLandBirds);
+const waterLandBirds = "Phalacrocorax corbo";
+("Gavia stellata");
+("Branta bernicle");
+("Aix galericulata");
+("Aythya fuligula");
+console.log(waterLandBirds);
