85aa79d4b7a48b4afd580f85f1bbe76ac3fa2e5b
Gabija <gabija.arm@gmail.com>
2020-09-23 11:27:38 +0300

    minor-changes

--- js/custom.js
+++ js/custom.js
@@ -16,21 +16,33 @@ const unknownText = "N";
 console.log(unknownText);
 
 // 3-4 Kintamųjų sąrašo sudarymas
-const forestBirds = "Ciconia nigra";
-("Buteo buteo");
-("Columba palumbus");
-("Cuculus canorus");
-("Otus scops");
+const forestBirds = [14, 30, 60, 40, 2];
+console.log(forestBirdsWeek);
+const grassLandsBirds = [13, 15, 6, 70, 3];
+console.log(grassLandsBirds);
+const waterLandBirds = [55, 30, 16, 4, 78];
+
+const forestBirds = [
+  "Ciconia nigra",
+  "Buteo buteo",
+  "Columba palumbus",
+  "Cuculus canorus",
+  "Otus scops",
+];
 console.log(forestBirds);
-const grassLandBirds = "Perdix perdix";
-("Bonasa bonasia");
-("Tetrao tetrix");
-("Syrrhaptes paradoxus");
-("Upupa epops");
+const grassLandBirds = [
+  "Perdix perdix",
+  "Bonasa bonasia",
+  "Tetrao tetrix",
+  "Syrrhaptes paradoxus",
+  "Upupa epops",
+];
 console.log(grassLandBirds);
-const waterLandBirds = "Phalacrocorax corbo";
-("Gavia stellata");
-("Branta bernicle");
-("Aix galericulata");
-("Aythya fuligula");
+const waterLandBirds = [
+  "Phalacrocorax corbo",
+  "Gavia stellata",
+  "Branta bernicle",
+  "Aix galericulata",
+  "Aythya fuligula",
+];
 console.log(waterLandBirds);
